d0418bbe8a11016bba6f0d6481279f6e29190b52
Jiachen Li <alexjiachenli@hotmail.com>
2021-04-08 22:04:56 +1000

    3

--- main.py
+++ main.py
@@ -2,38 +2,24 @@ import pandas as pd
 import numpy as np
 
 class Pose:
-    def __init__(self, pose, x):
+    def __init__(self, pose, mean, std, prior):
         self.pose = pose
-        self.x = data
-        self.prior = 0
+        self.mean = mean
+        self.std = std
+        self.prior
 
 
 def preprocess(filename):
-    y = []
-    x = []
-    f = open("train.csv",'r')
-    for line in f.readlines():
-        atts = line.strip().split(',')
-        y.append(atts[0])
-        floatX = []
-        for att in atts[1:]:
-            floatX.append(float(att))
-        x.append(floatX)
-    f.close()
-    return x,y
+    return pd.read_csv(filename, header = None).replace(9999, np.NaN)
 
-# def train(x, y):
-#     model = []
-#     data = []
-#     cur_pose = y[0]
-#     for i in range(len(x)):
-#         if y[i] != cur_pose:
-            
-#             model.append(Pose(cur_pose, data))
-#             data = []
-#         for j in range(len(x[i])):
-#             data[j].append(x[i][j])
-#     return model
+
+def train(data):
+    Model = []
+    labels = sorted(list(set(data[data.columns[0]])))
+    for label in labels:
+        pose_data = data.loc[data[0] == label]
+        pose_data = pose_data[pose_data.columns[1:]]
+        pose = Pose()
 
 
 filename = "train.csv"
@@ -41,12 +27,12 @@ filename = "train.csv"
 # train(x, y)
 
 a = pd.read_csv(filename, header = None)
+a = a.replace(9999, np.NaN)
 b = sorted(list(set(a[a.columns[0]])))
 c = a.loc[a[0] == b[0]]
 c = c[c.columns[1:]]
 # c = c.transpose()
-c = c.replace(9999, np.NaN)
 d = np.nanmean(c, axis=0)
 e = np.nanstd(c,axis= 0)
-print(e)
+print(preprocess(filename))
 
