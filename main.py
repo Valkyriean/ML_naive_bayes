162aa20459984aa37dbb877803cbc492e7f0ddc6
Jiachen Li <alexjiachenli@hotmail.com>
2021-04-09 20:41:34 +1000

    231

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
 import pandas as pd
 import numpy as np
-
-class Pose:
-    def __init__(self, pose, mean, std, prior):
-        self.pose = pose
-        self.mean = mean
-        self.std = std
-        self.prior = prior
-
+from scipy.stats import norm
 
 def preprocess(filename):
     return pd.read_csv(filename, header = None).replace(9999, np.NaN)
 
 
 def train(data):
-    Model = []
+    model = []
     labels = sorted(list(set(data[data.columns[0]])))
     for label in labels:
         pose_data = data.loc[data[0] == label]
         pose_data = pose_data[pose_data.columns[1:]]
-        pose = Pose(label, 
-                    np.nanmean(pose_data, axis=0),
-                    np.nanstd(pose_data, axis=0),
-                    len(pose_data)/len(data)
-                    )
+        norm_list = []
+        mean_list = np.nanmean(pose_data, axis=0)
+        std_list = np.nanstd(pose_data, axis=0)
+        for i in range(len(mean_list)):
+            norm_list.append(norm(mean_list[i], std_list[i]))
+        prior = len(pose_data)/len(data)
+        pose = [label, 
+                prior,
+                norm_list
+                ]
 
+        model.append(pose)
+    return model
+
+def predict(test, model):
+    predictions = []
+    data = test[test.columns[1:]]
+    for index, instance in data.iterrows():
+        prediction = ""
+        best_score = 0
+        first = True
+        for pose in model:
+            score = np.log(pose[1])
+            for i in range(len(instance)):
+                if np.isnan(instance.iloc[i]): 
+                    continue
+                score += pose[2][i].logpdf(instance.iloc[i])
+            if first is True:
+                best_score = score
+                prediction = pose[0]
+                first = False
+            elif score > best_score:
+                best_score = score
+                prediction = pose[0]
+        predictions.append(prediction)
+    return predictions
+
+def evaluate(predictions, test):
+    label = data[data.columns[0]]
+    total = len(predictions)
+    correct = 0
+    for i in range(len(predictions)):
+        if label[i] == predictions[i]:
+            correct+=1
+    return correct/total
 
 filename = "train.csv"
-# x, y = preprocess(filename)
-# train(x, y)
-
-a = pd.read_csv(filename, header = None)
-a = a.replace(9999, np.NaN)
-b = sorted(list(set(a[a.columns[0]])))
-c = a.loc[a[0] == b[0]]
-c = c[c.columns[1:]]
-# c = c.transpose()
-d = np.nanmean(c, axis=0)
-e = np.nanstd(c,axis= 0)
-print(len(c))
-
+data = preprocess(filename)
+model = train(data)
+test = preprocess("test.csv")
+predictions = predict(test, model)
+acc = evaluate(predictions, test)
+print(acc)
